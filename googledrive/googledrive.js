5e076df9adfd193be129bc66d68ffb3e190f0634
Knarf64 <franck.arrecot@gmail.com>
2013-03-06 13:03:47 +0100

    --

--- googledrive/googledrive.js
+++ googledrive/googledrive.js
@@ -176,8 +176,8 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
 				qid: qid,
 				results: results
 			};
-			Tomahawk.log("google drive search query : " + searchString +" , result: " + DumpObjectIndented(return_songs.results));
-			Tomahawk.addTrackResults(return_songs); 
+			//Tomahawk.log("google drive search query returned: ");
+			Tomahawk.addTrackResults(return_songs);
 	   });
     },
     
@@ -189,7 +189,7 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
 				qid: qid,
 				artists: results
 			};
-            Tomahawk.log("google drive artists returned: ");
+            //Tomahawk.log("google drive artists returned: ");
             //Tomahawk.addArtistResults(return_artists);
 		});
     },
@@ -203,7 +203,7 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
                 artist: artist,
                 albums: results
             };
-            Tomahawk.log("google drive albums returned: ");
+            //Tomahawk.log("google drive albums returned: ");
             //Tomahawk.addAlbumResults(return_albums);
         });         
     },
@@ -218,7 +218,7 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
                 album: album,
                 results: results
             };
-            Tomahawk.log("Google Drive tracks returned:");
+            //Tomahawk.log("Google Drive tracks returned:");
             //Tomahawk.addAlbumTrackResults(return_tracks);
 		});
     },
@@ -233,14 +233,16 @@ var GoogleDriveResolver = Tomahawk.extend(TomahawkResolver, {
 	googleDriveMusicManagerTests: function() {	 
 		 musicManagerTester.flushDatabaseTest() ;
 		 musicManagerTester.init() ;
-		 //musicManagerTester.populateDatabase(9) ;
+		 //musicManagerTester.addTrackTest() ;
+		// musicManager.deletionWithoutKeyTest();
+		 musicManagerTester.populateDatabase(9) ;
 		 //musicManagerTester.searchQueryTest() ;
 		 //~ musicManagerTester.resolveTest() ;
 		 //~ musicManagerTester.allArtistsQueryTest() ;
 		 //~ musicManagerTester.tracksQueryTest() ;
 		 //~ musicManagerTester.albumsQueryTest() ;		 		 
 		 //musicManagerTester.searchQueryTest() ;
-		 //musicManagerTester.showDatabase() ;
+		 musicManagerTester.showDatabase() ;
 	},
 
     onID3TagCallback: function(tags)
